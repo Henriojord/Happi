fd9a8a39de007478ecabd0911119a99fa22e1f85
henriojord <henriojord@eisti.eu>
2018-06-12 14:45:50 +0900

    add output serializer

--- classifiers/models/simpleclassifier/serialize_output.py
+++ classifiers/models/simpleclassifier/serialize_output.py
@@ -46,7 +46,7 @@ for t in testset:
         likelihood = torch.nn.functional.softmax(logits, 1)
         _, classe = torch.max(likelihood, 1)
 
-        if classe[0] == testset.classes[species]:
+        if classe[0] == testset.classes['species']:
             name = testset.data[i][0].split('/')[-1][:-4]
             with open(name, 'wb') as f:
                 pickle.dump(logits.cpu().detach().numpy(), f)
