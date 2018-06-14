9c579b7133291da5aeb8e36172ea2fb732baee6c
henriojord <henriojord@eisti.eu>
2018-06-14 15:38:22 +0900

    print probs

--- classifiers/models/simpleclassifier/serialize_output.py
+++ classifiers/models/simpleclassifier/serialize_output.py
@@ -56,7 +56,7 @@ for i_batch, sample in enumerate(dataloader):
         #If model's answer is correct and the species is the one specified as argument
         if classe[c].item() == sample['label'][c].item() and sample['species'][c] == args.species:
             print(likelihood.shape)
-            output = {k:(v, likelihood[c][v]) for k, v in testset.classes.items()}
+            output = {k:(v, likelihood[c][v].item()) for k, v in testset.classes.items()}
             print(output)
             # with open(os.path.join(args.directory, 'serialized_logits', args.species, sample['name'][c]), 'wb') as f:
             #     pickle.dump(logits.cpu().detach().numpy(), f)
